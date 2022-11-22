db1c7aa314b88f0c5d81c7ef5f81fab6fcfac478
Senak9 <joaomarcelo.o.rocha@gmail.com>
2022-11-21 21:51:57 -0300

    feat: cadastro de usuários

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { cadastrarUsuario } = require('./controladores/usuario');
 
 
 const rotas = express();
 
-
+rotas.post('/usuario', cadastrarUsuario);
 
 module.exports = rotas
