aee756acdb09b0e8632fd33ccfd340da0e308d2e
Senak9 <joaomarcelo.o.rocha@gmail.com>
2022-11-23 16:17:32 -0300

    feat: criando filtro de autenticação

--- src/rotas.js
+++ src/rotas.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { login } = require('./controladores/login');
 const { cadastrarUsuario } = require('./controladores/usuario');
+const { filtroAutenticacao } = require('./intermediarios/autenticacao');
 
 
 const rotas = express();
 
 rotas.post('/usuario', cadastrarUsuario);
+rotas.post('/login', login);
+
+rotas.use(filtroAutenticacao);
 
 module.exports = rotas
