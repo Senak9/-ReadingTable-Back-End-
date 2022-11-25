00d5927b94252369e55914493130ebaba5bf0f76
Senak9 <joaomarcelo.o.rocha@gmail.com>
2022-11-25 17:23:54 -0300

    git commit -m "feat: criando lista de categoria"

--- src/rotas.js
+++ src/rotas.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { listarCategorias } = require('./controladores/categoria');
 const { login } = require('./controladores/login');
 const { cadastrarUsuario, obterPerfilUsuario, atualizarPerfilUsuario } = require('./controladores/usuario');
 const { filtroAutenticacao } = require('./intermediarios/autenticacao');
@@ -13,5 +14,7 @@ rotas.use(filtroAutenticacao);
 
 rotas.get('/usuario', obterPerfilUsuario);
 rotas.put('/usuario', atualizarPerfilUsuario);
+
+rotas.get('/categoria', listarCategorias);
 
 module.exports = rotas
