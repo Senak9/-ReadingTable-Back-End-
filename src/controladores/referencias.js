2dd29eb1480a3943d32e95a48b2963eabc150d18
Senak9 <joaomarcelo.o.rocha@gmail.com>
2022-11-26 17:24:29 -0300

    fix: modificano lista de referencia

--- src/controladores/referencias.js
+++ src/controladores/referencias.js
@@ -1,8 +1,10 @@
 const { query } = require("../bancodedados/conexao");
 
 const listarReferencias = async (req, res) => {
+    const { usuario } = req;
+
     try {
-        const referencias = await query('select * from referencias');
+        const referencias = await query('select * from referencias where usuario_id = $1', [usuario.id]);
         return res.json(referencias.rows);
     } catch (error) {
         return res.status(500).json({ mensagem: `Erro interno: ${error.message}` });
