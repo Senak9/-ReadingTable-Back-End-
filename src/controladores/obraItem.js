9444357ab73394cb3f47d67803171431596bfbfb
Senak9 <joaomarcelo.o.rocha@gmail.com>
2022-11-26 17:13:37 -0300

    feat: criando lista de obras

--- src/controladores/obraItem.js
+++ src/controladores/obraItem.js
@@ -2,15 +2,26 @@ const { query } = require("../bancodedados/conexao");
 
 const listaObraItem = async (req, res) => {
     const { usuario } = req;
+    const { filtro } = req.query;
+
+    if (filtro && !Array.isArray(filtro)) {
+        return res.status(400).json({ mensagem: 'O filtro precisa ser um array' });
+    }
 
     try {
         let queryLike = '';
+        let arrayFiltro;
+
+        if (filtro) {
+            arrayFiltro = filtro.map((item) => `%${item}%`);
+            queryLike += `and c.nome ilike any($2)`;
+        }
 
         const queryObraItem = `
-            select t.*, c.descricao as categoria_nome from items t 
+            select i.*, c.nome as categoria_nome from items i 
             left join categorias c 
-            on t.categoria_id = c.id 
-            where t.usuario_id = $1 
+            on i.categoria_id = c.id 
+            where i.usuario_id = $1 
             ${queryLike}
         `;
 
